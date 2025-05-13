b163f1432c6ee31b1286807300fbaed4135fc529
Mesut Kaya <mesutkaya2000@gmail.com>
2025-05-13 08:39:27 +0300

    Add project files.

---
+++ Kazandrio-Application/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using FluentScheduler;
+using Kazandrio_Application.Jobs;
+
+namespace Kazandrio_Application
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var registry = new Registry();
+
+            // İşin ne sıklıkla çalışacağını belirtiyoruz (örneğin her gün saat 12'de)
+            //registry.Schedule<AppiumTestJob>().ToRunEvery(1).Days().At(12, 0); // Her gün 12'de çalışacak
+            registry.Schedule<AppiumTestJob>().ToRunNow(); // Her gün 12'de çalışacak
+
+            // FluentScheduler'ı başlatıyoruz
+            JobManager.Initialize(registry);
+        }
+    }
+}
---
+++ Kazandrio-Application/Jobs/AppiumTestJob.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentScheduler;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace Kazandrio_Application.Jobs
+{
+    public class AppiumTestJob : IJob
+    {
+        public void Execute()
+        {
+            try
+            {
+                // Appium kodunuzu burada çalıştırabilirsiniz.
+                Console.WriteLine("Appium testi başlatılıyor...");
+
+                var options = new AppiumOptions();
+                options.PlatformName = "Android";
+                options.DeviceName = "tghemfszhbijn4h8l?ss";
+                options.PlatformVersion = "10.0";
+                options.AddAdditionalAppiumOption("appPackage", "com.pepsico.kazandirio");
+                options.AddAdditionalAppiumOption("appActivity", ".MainActivity");
+
+                AndroidDriver driver = new AndroidDriver(new Uri("http://127.0.0.1:4723/wd/hub"), options);
+
+                IWebElement okutKazanButton = driver.FindElement(By.Id("com.pepsico.kazandirio:id/navigation_bar_item_labels_group"));
+                okutKazanButton.Click();
+
+                IWebElement toolbarTitle = driver.FindElement(By.Id("com.pepsico.kazandirio:id/text_view_toolbar_title"));
+                toolbarTitle.Click();
+                IWebElement passwordField = driver.FindElement(By.Id("com.pepsico.kazandirio:id/text_view_manual_code_write_enter_code"));
+                passwordField.SendKeys("12345678910");
+
+                IWebElement tamamButton = driver.FindElement(By.Id("com.pepsico.kazandirio:id/button_manual_code_write_ok"));
+                tamamButton.Click();
+
+                Console.WriteLine("Şifre başarıyla girildi.");
+
+                driver.Quit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata: " + e.Message);
+            }
+        }
+    }
+}
